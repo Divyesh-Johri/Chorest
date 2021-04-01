a294f23e8a2887f90aa3170af689fe421cd9f5f9
Bhero122 <cvm5769@psu.edu>
2021-04-01 16:02:10 -0400

    Modified Radio Button Group

--- app/src/main/java/com/example/chorest_app/AddChorestActivity.java
+++ app/src/main/java/com/example/chorest_app/AddChorestActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class AddChorestActivity extends AppCompatActivity {
 
@@ -23,10 +24,35 @@ public class AddChorestActivity extends AppCompatActivity {
         rbCurrentLocation = findViewById(R.id.rbCurrentLocation);
         rbChooseLocation = findViewById(R.id.rbChooseLocation);
 
+        //RadioButton radioButton = (RadioButton) findViewById(R.id.rbCurrentLocation);
+
+        /*rbCurrentLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });*/
+
 
     }
 
     public void onRadioButtonClicked(View view){
-        //boolean checked = ((rbCurrentLocation) view).isChecked();
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()){
+            case R.id.rbCurrentLocation:
+                if(checked)
+                    Toast.makeText(AddChorestActivity.this, "Current Location", Toast.LENGTH_SHORT).show();
+                    // Implement GPS location feature
+                    // Address text view is greyed out
+                    break;
+
+            case R.id.rbChooseLocation:
+                if(checked)
+                    Toast.makeText(AddChorestActivity.this, "Choose A Location", Toast.LENGTH_SHORT).show();
+                    // Address text view can be used
+                    // Implement Google Maps api
+                    break;
+        }
     }
 }
