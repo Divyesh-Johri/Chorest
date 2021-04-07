c82b74b401fe23689f5b0516e0a1c9d7c325386b
Divyesh Johri <divyeshjohri@gmail.com>
2021-04-06 22:14:34 -0400

    Added https call for Places text search

--- app/src/main/java/com/example/chorest_app/Chorest.java
+++ app/src/main/java/com/example/chorest_app/Chorest.java
@@ -4,6 +4,9 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.RequestParams;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -13,11 +16,15 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import okhttp3.Headers;
 
 
 public class Chorest {
@@ -28,7 +35,8 @@ public class Chorest {
     public String id;
     public String name;
     public Timestamp timestamp;
-    public String location;
+    public double location_latitude;
+    public double location_longitude;
     public String[] chores;
     public String[] route;
 
@@ -36,8 +44,11 @@ public class Chorest {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     // Current User
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    // Async HTTP Client
+    AsyncHttpClient httpClient = new AsyncHttpClient();
     // User ID
     String uID = user.getUid();
+    // chorests collection path
     String currentPath = "users/" + uID + "/chorests/";
 
 
@@ -62,7 +73,8 @@ public class Chorest {
                         Map<String, Object> chorestDoc = document.getData();
                         name = (String) chorestDoc.get("name");
                         timestamp = (Timestamp) chorestDoc.get("timestamp");
-                        location = (String) chorestDoc.get("location");
+                        location_latitude = (Double) chorestDoc.get("location_latitude");
+                        location_longitude = (Double) chorestDoc.get("location_longitude");
                         chores = (String[]) chorestDoc.get("chores");
                         route = (String[]) chorestDoc.get("route");
                     } else {
@@ -77,10 +89,14 @@ public class Chorest {
 
     // Updates chorest document in Firestore with newly set values
     private void updateChorest() {
+        // Timestamp updated in document BUT NOT IN OBJ (object would need to be recreated)
+        FieldValue currentTimestamp = FieldValue.serverTimestamp();
+        // Specify fields
         Map<String, Object> chorestDoc = new HashMap<>();
         chorestDoc.put("name", this.name);
-        chorestDoc.put("timestamp", this.timestamp);
-        chorestDoc.put("location", this.location);
+        chorestDoc.put("timestamp", currentTimestamp);
+        chorestDoc.put("location_latitude", this.location_latitude);
+        chorestDoc.put("location_longitude", this.location_longitude);
         chorestDoc.put("chores", this.chores);
         chorestDoc.put("route", this.route);
 
@@ -100,6 +116,44 @@ public class Chorest {
                 });
     }
 
+    // Search for places near specified location
+    private ArrayList<String[]> retrievePlaces() {
+        // List where each element is a list of addresses for each chore
+        ArrayList<String[]> places = new ArrayList<>();
+
+        // Find addresses of locations for each chore
+        String httpAddress = "https://maps.googleapis.com/maps/api/place/textsearch/json?";
+        for (String chore : this.chores){
+            // Set parameters for chore
+            RequestParams params = new RequestParams();
+            params.put("query", chore);
+            params.put("location", "" + this.location_latitude + "," + this.location_longitude);
+            params.put("fields", "formatted_address");
+            params.put("radius", "100");
+            params.put("key", R.string.places_api_key);
+
+            httpClient.get(httpAddress, params, new JsonHttpResponseHandler() {
+                @Override
+                public void onSuccess(int i, Headers headers, JSON json) {
+                    Log.d(TAG, "retrievePlaces - Addresses received: " + json.jsonObject.toString());
+                    // Add json addresses in places as a String[]
+                }
+
+                @Override
+                public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+                    Log.e(TAG, "retrievePlaces - Failed to retrieve addresses: ", throwable);
+                }
+            });
+        }
+
+        return places;
+    }
+    // Find shortest distance between 2 locations
+    // Create graph with addresses
+    // Primm's algorithm
+    // Calculate shortest route action method
+
+
     // Getters and Setters
 
     public void setName(String name) {
@@ -107,8 +161,9 @@ public class Chorest {
         updateChorest();
     }
 
-    public void setLocation(String location) {
-        this.location = location;
+    public void setLocation(double latitude, double longitude) {
+        this.location_latitude = latitude;
+        this.location_longitude = longitude;
         updateChorest();
     }
 
@@ -125,12 +180,16 @@ public class Chorest {
         return name;
     }
 
-    public Timestamp getTimestamp() {
-        return timestamp;
+    public double getLocation_latitude() {
+        return location_latitude;
     }
 
-    public String getLocation() {
-        return location;
+    public double getLocation_longitude() {
+        return location_longitude;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
     }
 
     public String[] getChores() {
