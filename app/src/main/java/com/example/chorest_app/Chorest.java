ce6ec15a6d2f04020c419db8cad19a3f30bd0cf0
Divyesh Johri <divyeshjohri@gmail.com>
2021-04-11 13:48:53 -0400

    Chorest modified after testing
    Needs better support for asynchronous calls

--- app/src/main/java/com/example/chorest_app/Chorest.java
+++ app/src/main/java/com/example/chorest_app/Chorest.java
@@ -15,10 +15,8 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -31,44 +29,81 @@ public class Chorest {
 
     public static final String TAG = "Chorest Object";
 
+    // Static property of all of user's chorests
+    public static ArrayList<Chorest> allUserChorests;
+
     // Properties for Chorest object
     private String id;
     private String name;
     private Timestamp timestamp;
     private double location_latitude;
     private double location_longitude;
-    private String[] chores;
-    private String[] route;
-    private String[] route_addresses;
+    private ArrayList<String> chores;
+    private ArrayList<String> route;
+    private ArrayList<String> route_addresses;
     private int route_distance;
     private boolean find_new_route;
 
     // Firestore database initialization
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static FirebaseFirestore db = FirebaseFirestore.getInstance();
     // Current User
-    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private static FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     // User ID
-    String uID = user.getUid();
-    // chorests collection path
-    String currentPath = "users/" + uID + "/chorests/";
+    private static String uID = user.getUid();
+    // Chorests collection path
+    private static String currentPath = "users/" + uID + "/chorests/";
 
 
 
     // Constructor for retrieving existing Chorest
     public Chorest(String id) {
         this.id = id;
+        Log.i(TAG, "RETRIEVING EXISTING CHOREST");
         findChorest();
     }
 
     // Constructor for initializing new Chorest
     // Requires name of chorest, latitude and longitude of user's specified location, and a list of chores
-    public Chorest(String name, double loc_lat, double loc_long, String[] chores){
+    public Chorest(String name, double loc_lat, double loc_long, ArrayList<String> chores){
         this.name = name;
         this.location_latitude = loc_lat;
         this.location_longitude = loc_long;
         this.chores = chores;
         this.find_new_route = false;
+        Log.i(TAG, "CREATING CHOREST");
         createChorest();
+    }
+
+    // Define information for existing Chorest
+    // Used by getAllUserChorests
+    public Chorest(String id, String name, Timestamp timestamp,
+                   double location_latitude, double location_longitude,
+                   ArrayList<String> chores, ArrayList<String> route, ArrayList<String> route_addresses,
+                   int route_distance, boolean find_new_route) {
+        this.id = id;
+        this.name = name;
+        this.timestamp = timestamp;
+        this.location_latitude = location_latitude;
+        this.location_longitude = location_longitude;
+        this.chores = chores;
+        this.route = route;
+        this.route_addresses = route_addresses;
+        this.route_distance = route_distance;
+        this.find_new_route = find_new_route;
+    }
+
+    // Define information for chorests with no routes
+    // Used by getAllUserChorests
+    public Chorest(String id, String name, Timestamp timestamp,
+                   double location_latitude, double location_longitude,
+                   ArrayList<String> chores, boolean find_new_route) {
+        this.id = id;
+        this.name = name;
+        this.timestamp = timestamp;
+        this.location_latitude = location_latitude;
+        this.location_longitude = location_longitude;
+        this.chores = chores;
+        this.find_new_route = find_new_route;
     }
 
     // Creates new chorest in Firestore
@@ -81,6 +116,8 @@ public class Chorest {
         chorestDoc.put("location_longitude", this.location_longitude);
         chorestDoc.put("chores", this.chores);
         chorestDoc.put("find_new_route", this.find_new_route);
+
+        Log.i(TAG, "createChorest -- INFO NOW BEING ADDED");
 
         db.collection(currentPath)
                 .add(chorestDoc)
@@ -117,10 +154,12 @@ public class Chorest {
                         timestamp = (Timestamp) chorestDoc.get("timestamp");
                         location_latitude = (Double) chorestDoc.get("location_latitude");
                         location_longitude = (Double) chorestDoc.get("location_longitude");
-                        chores = (String[]) chorestDoc.get("chores");
-                        route = (String[]) chorestDoc.get("route");
-                        route_addresses = (String[]) chorestDoc.get("route_addresses");
-                        route_distance = (Integer) chorestDoc.get("route_distance");
+                        chores = (ArrayList<String>) chorestDoc.get("chores");
+                        if(chorestDoc.get("route") != null){
+                            route = (ArrayList<String>) chorestDoc.get("route");
+                            route_addresses = (ArrayList<String>) chorestDoc.get("route_addresses");
+                            route_distance = (Integer) chorestDoc.get("route_distance");
+                        }
                         find_new_route = (Boolean) chorestDoc.get("find_new_route");
                     } else {
                         Log.d(TAG, "findChorest - No such document");
@@ -166,6 +205,7 @@ public class Chorest {
                 });
     }
 
+    // Will set new routes once they have finished updating
     private void getCurrentRoutes() {
         DocumentReference docRef = db.collection(currentPath).document(this.id);
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -180,12 +220,13 @@ public class Chorest {
                         find_new_route = (Boolean) chorestDoc.get("find_new_route");
 
                         // If values haven't been updated, try again
+                        // THIS IS BAD, SHOULDN"T EXIST WILL MAKE APP CRASH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                         if (find_new_route){
                             getCurrentRoutes();
                         }
 
-                        route = (String[]) chorestDoc.get("route");
-                        route_addresses = (String[]) chorestDoc.get("route_addresses");
+                        route = (ArrayList<String>) chorestDoc.get("route");
+                        route_addresses = (ArrayList<String>) chorestDoc.get("route_addresses");
                         route_distance = (Integer) chorestDoc.get("route_distance");
 
                     } else {
@@ -221,6 +262,62 @@ public class Chorest {
         });
     }
 
+
+
+    // Callable Public methods
+
+    // Public static method to receive all Chorest objects for a user
+    // How to use:
+    // Chorest.getAllUserChorests();
+    // ArrayList<Chorests> var = Chorest.allUserChorests;
+    public static void getAllUserChorests(){
+        db.collection(currentPath)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, "getAllUserChorests - " + document.getId() + " => " + document.getData());
+                                // Skip iteration of document if Chorest is invalid
+                                if (document.getId() == uID + "ROUTE" ){
+                                    continue;
+                                }
+                                else{
+                                    Map<String, Object> chorestDoc = document.getData();
+                                    if(chorestDoc.get("route") != null){
+                                        allUserChorests.add(new Chorest(
+                                                document.getId(),
+                                                (String) chorestDoc.get("name"),
+                                                (Timestamp) chorestDoc.get("timestamp"),
+                                                (Double) chorestDoc.get("location_latitude"),
+                                                (Double) chorestDoc.get("location_longitude"),
+                                                (ArrayList<String>) chorestDoc.get("chores"),
+                                                (ArrayList<String>) chorestDoc.get("route"),
+                                                (ArrayList<String>) chorestDoc.get("route_addresses"),
+                                                (Integer) chorestDoc.get("route_distance"),
+                                                (Boolean) chorestDoc.get("find_new_route") ));
+                                    }
+                                    else{
+                                        allUserChorests.add(new Chorest(
+                                                document.getId(),
+                                                (String) chorestDoc.get("name"),
+                                                (Timestamp) chorestDoc.get("timestamp"),
+                                                (Double) chorestDoc.get("location_latitude"),
+                                                (Double) chorestDoc.get("location_longitude"),
+                                                (ArrayList<String>) chorestDoc.get("chores"),
+                                                (Boolean) chorestDoc.get("find_new_route") ));
+                                    }
+
+                                }
+                            }
+                        } else {
+                            Log.d(TAG, "getAllUserChorests - Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
     // Trigger to calculate new routes
     public void findNewRoute(){
         this.find_new_route = true;
@@ -229,6 +326,7 @@ public class Chorest {
 
     // Returns true when route fields are updated
     // Meant for use after calling findNewRoute()
+    // THIS IS BAD, SHOULDN"T EXIST WILL MAKE APP CRASH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     public Boolean isRouteUpdated(){
         if (this.find_new_route){
             return false;
@@ -237,6 +335,7 @@ public class Chorest {
             return true;
         }
     }
+
 
 
     // Getters and Setters
@@ -253,7 +352,7 @@ public class Chorest {
         updateChorest();
     }
 
-    public void setChores(String[] chores) {
+    public void setChores(ArrayList<String> chores) {
         this.chores = chores;
         updateChorest();
     }
@@ -280,15 +379,15 @@ public class Chorest {
         return timestamp;
     }
 
-    public String[] getChores() {
+    public ArrayList<String> getChores() {
         return chores;
     }
 
-    public String[] getRoute() {
+    public ArrayList<String> getRoute() {
         return route;
     }
 
-    public String[] getRoute_addresses() {
+    public ArrayList<String> getRoute_addresses() {
         return route_addresses;
     }
 
