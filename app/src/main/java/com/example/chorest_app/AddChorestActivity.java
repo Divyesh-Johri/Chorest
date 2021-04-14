1031acaa5950d06968c1d91d9b17a955226a8615
Lakshmanan Sockalingam <laksh2145@gmail.com>
2021-04-14 16:32:42 +0530

    Location

--- app/src/main/java/com/example/chorest_app/AddChorestActivity.java
+++ app/src/main/java/com/example/chorest_app/AddChorestActivity.java
@@ -3,12 +3,19 @@ package com.example.chorest_app;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -25,6 +32,11 @@ import com.example.chorest_app.Fragments.HomeFragment;
 import com.example.chorest_app.Fragments.MapFragment;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -36,7 +48,7 @@ import com.google.firebase.firestore.Query;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AddChorestActivity extends AppCompatActivity {
+public class AddChorestActivity<True> extends AppCompatActivity {
 
     private static final String TAG = "AddChorestActivity";
     private RadioGroup rgLocation;
@@ -46,20 +58,30 @@ public class AddChorestActivity extends AppCompatActivity {
     private EditText etChorestName;
     private RecyclerView rvCalculatedRoutes;
     private Button btTypeAddress;
-    private static final int  RC_TO_MAP  = 23;
+    private Button btAddLocations;
+    private static final int RC_TO_MAP = 23;
 
-    private static final String  KEY_CHOREST_NAME = "name";
+    private static final String KEY_CHOREST_NAME = "name";
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirestoreRecyclerAdapter adapter;
+
+    private double longitude;
+    private double latitude;
+    private boolean location;
+    private LocationRequest mLocationRequest;
+
+    private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
+    private long FASTEST_INTERVAL = 2000; /* 2 sec */
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_chorest);
-
+        startLocationUpdates();
+        location = true;
         mAuth = FirebaseAuth.getInstance();
         rgLocation = findViewById(R.id.rgLocation);
         rbCurrentLocation = findViewById(R.id.rbCurrentLocation);
@@ -67,7 +89,65 @@ public class AddChorestActivity extends AppCompatActivity {
         btCalculateMap = findViewById(R.id.btCalculateMap);
         etChorestName = findViewById((R.id.etChorestName));
         rvCalculatedRoutes = findViewById(R.id.rvCalculatedRoutes);
-        btTypeAddress =  findViewById(R.id.btTypeAddress);
+        btTypeAddress = findViewById(R.id.btTypeAddress);
+        btAddLocations = findViewById(R.id.btAddLocations);
+
+        //Location
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE); //fine location - gps
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//        double longitude = location.getLongitude();
+//        double latitude = location.getLatitude();
+//        //Location
+//
+//        if (Location == null) {
+//
+//        }
+
+
+        final LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                longitude = -77.859909;
+                latitude = 40.8148;
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            ActivityCompat.(requestPermissions);
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
 
 
 
@@ -111,6 +191,22 @@ public class AddChorestActivity extends AppCompatActivity {
             }
         });
 
+        btAddLocations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        private final LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+            }
+        }
+
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
+
 
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
@@ -152,6 +248,92 @@ public class AddChorestActivity extends AppCompatActivity {
 
     }
 
+    protected void startLocationUpdates() {
+        // Create the location request to start receiving updates
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        // Create LocationSettingsRequest object using location request
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        // Check whether location settings are satisfied
+        // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        settingsClient.checkLocationSettings(locationSettingsRequest);
+
+        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
+        getFusedLocationProviderClient(this).requestLocationUpdates(mLocationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        // do work here
+                        onLocationChanged(locationResult.getLastLocation());
+                    }
+                },
+                Looper.myLooper());
+    }
+
+    public void onLocationChanged(Location location) {
+        // New location has now been determined
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        // You can now create a LatLng Object for use with maps
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+    }
+
+    public void getLastLocation() {
+        // Get last known recent location using new Google Play Services SDK (v11+)
+        FusedLocationProviderClient locationClient = getFusedLocationProviderClient(this);
+
+        locationClient.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // GPS location can be null if GPS is switched off
+                        if (location != null) {
+                            onLocationChanged(location);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("MapDemoActivity", "Error trying to get last GPS location");
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        if(checkPermissions()) {
+            googleMap.setMyLocationEnabled(true);
+        }
+    }
+
+    private boolean checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            requestPermissions();
+            return false;
+        }
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                REQUEST_FINE_LOCATION);
+    }
+    //Location
+
     public void onRadioButtonClicked(View view){
         boolean checked = ((RadioButton) view).isChecked();
 
@@ -159,6 +341,8 @@ public class AddChorestActivity extends AppCompatActivity {
             case R.id.rbCurrentLocation:
                 if(checked)
                     Toast.makeText(AddChorestActivity.this, "Current Location", Toast.LENGTH_SHORT).show();
+                    location = true;
+                    //get longitude and latitude from current gps
                     // Implement GPS location feature
                     // Address text view is greyed out
                     break;
@@ -166,6 +350,7 @@ public class AddChorestActivity extends AppCompatActivity {
             case R.id.rbChooseLocation:
                 if(checked)
                     Toast.makeText(AddChorestActivity.this, "Choose A Location", Toast.LENGTH_SHORT).show();
+                    location = false;
                     // Address text view can be used
                     // Implement Google Maps api
                     break;
